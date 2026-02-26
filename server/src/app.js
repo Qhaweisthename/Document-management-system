9f7506762577aa817e7a76dc38876b325a5ee2d8
Qhawe Ngcongo <ST10283124@vcconnect.edu.za>
2026-02-26 12:03:38 +0200

    Remove problematic app.options line causing crash

--- server/src/app.js
+++ server/src/app.js
@@ -23,8 +23,7 @@ app.use(cors({
 }));
 
 
-// Handle preflight requests for all routes
-app.options('/*', cors());
+
 
 // ============ SIMPLIFIED OPTIONS HANDLER ============
 app.use((req, res, next) => {
